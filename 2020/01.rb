f17331bbaa7f35059c3deadb3b50149714bf8b6f
anim <anton@telpart.ru>
2021-01-20 07:44:13 +0300

    Added 01 day, part 2

--- 2020/01.rb
+++ 2020/01.rb
@@ -3,18 +3,36 @@
 input=`xclip -o`
 
 lines = input.lines.map(&:to_i)
-indexes = []
 
+indexes = []
 lines.each_with_index do |a, i|
+  next if indexes.include?(i)
   lines.each_with_index do |b, j|
-    next if i == j || indexes.include?(i) || indexes.include?(j)
+    next if i == j || indexes.include?(j)
     if a + b == 2020
       indexes << i
       indexes << j
     end
   end
 end
-
 answer = indexes.inject(1) {|v, i| v * lines[i]}
-puts "Answer: #{answer}"
+puts "Answer1: #{answer}"
+
+indexes = []
+lines.each_with_index do |a, i|
+  next if indexes.include?(i)
+  lines.each_with_index do |b, j|
+    next if i == j || indexes.include?(j)
+    lines.each_with_index do |c, k|
+      next if i == k || indexes.include?(k)
+      if a + b + c == 2020
+        indexes << i
+        indexes << j
+        indexes << k
+      end
+    end
+  end
+end
+answer = indexes.inject(1) {|v, i| v * lines[i]}
+puts "Answer2: #{answer}"
 
