b316403bdcd0b3f283ddd0be486c2add2e8b38a8
anim <anton@telpart.ru>
2024-01-05 01:12:46 +0300

    Cleaning code

--- 2023/02.rb
+++ 2023/02.rb
@@ -12,16 +12,34 @@ input = $stdin.readlines
 games = []
 input.each do |line|
   line.strip!
-  possible = true
   tokens_data = line.split(': ')
   tokens_game = tokens_data[0].split(' ')
-  game_id = tokens_game[1].to_i
+  game = { 
+    id: tokens_game[1].to_i,
+    cubes: []
+  }
+
   tokens_data = tokens_data[1].split('; ')
   tokens_data.each do |data|
+    cubes = {}
     tokens_cubes = data.split(', ')
     tokens_cubes.each do |data|
       tokens = data.split(' ')
-      if tokens[0].to_i > CUBES[tokens[1]]
+      cubes[tokens[1]] = tokens[0].to_i
+    end
+
+    game[:cubes] << cubes
+  end
+
+  games << game
+end
+
+possible_games = []
+games.each do |game|
+  possible = true
+  game[:cubes].each do |cubes|
+    cubes.each do |cube, value|
+      if value > CUBES[cube]
         possible = false
         break
       end
@@ -30,37 +48,32 @@ input.each do |line|
     break unless possible
   end
 
-  games << game_id if possible
+  next unless possible
+
+  possible_games << game[:id]
 end
 
-puts(games.sum)
+puts(possible_games.sum)
 
-games.clear
-cubes = {
+possible_games.clear
+cubes_max = {
   'red' => 0,
   'green' => 0,
   'blue' => 0
 }
 
-input.each do |line|
-  line.strip!
-  cubes.each_key do |cube|
-    cubes[cube] = 0
+games.each do |game|
+  cubes_max.each_key do |cube|
+    cubes_max[cube] = 0
   end
 
-  tokens_data = line.split(': ')
-  tokens_game = tokens_data[0].split(' ')
-  game_id = tokens_game[1].to_i
-  tokens_data = tokens_data[1].split('; ')
-  tokens_data.each do |data|
-    tokens_cubes = data.split(', ')
-    tokens_cubes.each do |data|
-      tokens = data.split(' ')
-      cubes[tokens[1]] = [cubes[tokens[1]], tokens[0].to_i].max
+  game[:cubes].each do |cubes|
+    cubes.each do |cube, value|
+      cubes_max[cube] = [cubes_max[cube], value].max
     end
   end
 
-  games << cubes.values.inject(1, :*)
+  possible_games << cubes_max.values.inject(1, :*)
 end
 
-puts(games.sum)
+puts(possible_games.sum)
